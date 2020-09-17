bbf12ec3838e2cf2365fad2af774398585920d0b
Xiyu Fan <fanxiyu@me.com>
2020-09-16 20:53:30 -0700

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by 范希宇 on 9/16/20.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
